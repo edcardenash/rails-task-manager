b0e26ed28095f7f3820faabae1eceb34b9d773e6
Edgar Cardenas <edcardenash@gmail.com>
2022-11-19 17:46:19 -0300

    Logica controladores y vista aplicada

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,9 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   get 'tasks', to: 'tasks#index'
-  get 'task/new', to: 'task#new', as: 'new_task'
-  get 'task/:id', to: 'task#show', as: 'task'
-  post 'tasks', to: 'task#create'
+  get 'tasks/new', to: 'tasks#new', as: 'new_task'
+  get 'tasks/:id', to: 'tasks#show', as: 'task'
+  post 'tasks', to: 'tasks#create'
   get 'tasks/:id/edit', to: 'tasks#edit', as: 'edit_task'
   patch 'tasks/:id', to: 'tasks#update'
   delete 'tasks/:id', to: 'tasks#destroy'
