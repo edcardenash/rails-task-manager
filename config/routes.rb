631c57f6e50299e4ae54044a93449be6e81dfe6c
Edgar Cardenas <edcardenash@gmail.com>
2022-11-19 15:39:38 -0300

    Creacion de rutas, controlador y views para modelo Task

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,11 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  get 'tasks', to: 'tasks#index'
+  get 'task/new', to: 'task#new', as: 'new_task'
+  get 'task/:id', to: 'task#show', as: 'task'
+  post 'tasks', to: 'task#create'
+  get 'tasks/:id/edit', to: 'tasks#edit', as: 'edit_task'
+  patch 'tasks/:id', to: 'tasks#update'
+  delete 'tasks/:id', to: 'tasks#destroy'
 end
